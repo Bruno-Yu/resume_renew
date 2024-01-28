aa34a110585b0769674601e6b46cf14368c42b39
Bruno Yu <jackhellowin@gmail.com>
2024-01-28 17:16:53 +0800

    feat(swiper): 加入 swiper & 對應邏輯

--- information.ts
+++ information.ts
@@ -48,3 +48,56 @@ export const bannerContent = {
     mobile: 'src/assets/images/avatar.webp',
   }
 }
+
+// works 
+
+export const works = [
+  {
+    cover_img: 'src/assets/images/work01.webp',
+    title: 'NFT GEL',
+    description: '販售 NFT 網站的仿做，嘗試刻劃的練習作品',
+    skills: 'ejs,gulp,Bootstrap5',
+    repo_url: 'https://github.com/Bruno-Yu/NFT_GEL.git',
+    page_url: 'https://bruno-yu.github.io/NFT_GEL/',
+  },
+  {
+    cover_img: 'src/assets/images/work02.webp',
+    title: 'GrandMaSam\nDiner',
+    description: '山姆大嬸餐館，獨樹一格的菜餚與經營方式，一定讓體驗過的您難以忘懷',
+    skills: 'Vue3,Vue_cli,Bootstrap5',
+    repo_url: 'https://github.com/Bruno-Yu/GrandMaSam_Diner.git',
+    page_url: 'https://bruno-yu.github.io/GrandMaSam_Diner/#/',
+  },
+  {
+    cover_img: 'src/assets/images/THEF2E.webp',
+    title: 'THE F2E\n2022',
+    description: 'THE F2E 2022 黑客松',
+    skills: 'ejs, gulp ,Tailwind, GSAP, AOS, Lottie',
+    repo_url: 'https://github.com/Bruno-Yu/TheF2E_2022_week1.git',
+    page_url: 'https://bruno-yu.github.io/TheF2E_2022_week1/',
+  },
+  {
+    cover_img: 'src/assets/images/點點簽.webp',
+    title: 'THE F2E\n2022',
+    description: 'THE F2E 2022 點點簽',
+    skills: 'Vue 3, Vue CLI, Pinia, Tailwind CSS, Tailwind Elements, pdf.js, fabric.js, jspdf.js',
+    repo_url: 'https://github.com/Bruno-Yu/TheF2E_2022_DotSign.git',
+    page_url: 'https://bruno-yu.github.io/TheF2E_2022_DotSign/#/',
+  },
+  {
+    cover_img: 'src/assets/images/work03.webp',
+    title: 'Coding\nHome',
+    description: '程式家教，切版練習',
+    skills: 'ejs, gulp ,Bootstrap5',
+    repo_url: 'https://github.com/Bruno-Yu/Coding_Home.git',
+    page_url: 'https://bruno-yu.github.io/Coding_Home/',
+  },
+  {
+    cover_img: 'src/assets/images/work04.webp',
+    title: 'Todo List',
+    description: '待辦清單，練習 Create React App 使用以及 RESTful API 串接',
+    skills: '串接 Swagger API, React Hook Form, useContext',
+    repo_url: 'https://github.com/Bruno-Yu/todolist_react.git',
+    page_url: 'https://bruno-yu.github.io/todolist_react/',
+  }
+]
