dbd94339c3d789178d137ba11a9d41793269cb17
Bruno Yu <jackhellowin@gmail.com>
2024-02-15 23:57:45 +0800

    fix(navbar): 1. 調整 navbar menu 2. 調整 information multiLanguage link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -20,17 +20,17 @@
           </li>
         </ul>
       </div>
-      <button type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" @click="toggleMenuDropdown">
+      <button data-collapse-toggle="navbar-menu" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
           <span class="sr-only">Open main menu</span>
           <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
           </svg>
       </button>
     </div>
-    <div ref="menuCollapse" class="hidden ml-auto mr-2 w-full md:block md:w-auto">
+    <div ref="menuCollapse" class="hidden ml-auto mr-2 w-full md:block md:w-auto" id="navbar-menu">
       <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li v-for="(item, index) in menuContent" :key="index" :class="{ 'hidden': item.hidden }">
-          <a  type="button" :href="item.link" class="block py-2 px-3 text-black hover:text-primary rounded md:bg-transparent md:p-0 " aria-current="page" @click="closeMenuTab">{{ isEng ? item.en_menuTab : item.menuTab }}</a>
+          <a  type="button" :href="item.link" class="block py-2 px-3 text-black hover:text-primary rounded md:bg-transparent md:p-0 " aria-current="page" >{{ isEng ? item.en_menuTab : item.menuTab }}</a>
         </li>
       </ul>
     </div>
